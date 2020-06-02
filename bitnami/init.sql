de374e85687552ab63541c8c977addc29a0c12c7
Sreekar Dhaduvai <sreekardhaduvai@Sreekars-MacBook-Pro.local>
2020-06-01 20:45:40 -0400

    sql init file

---
+++ bitnami/init.sql
@@ -0,0 +1,5 @@
+CREATE DATABASE test_db;
+use test_db;
+create table customer(userId int primary key, name varchar(40));
+create table restaurant(rid INTEGER primary key, name varchar(40));
+create table orders(uuid varchar(40) PRIMARY KEY, cust_id INTEGER, rest_id INTEGER, status varchar(20), dish varchar(20), FOREIGN KEY (cust_id) references customer(userId), FOREIGN KEY (rest_id) references restaurant(rid));
